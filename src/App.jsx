b7237909d553993e9abeebf8b2a7c6bdd38a5216
jinsung <treasure1209@naver.com>
2023-06-21 12:53:26 +0900

    Feat: 기본 레이아웃 설정

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,24 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="layout">
+      <header className="app-title">
+        <div>My Todo List</div> <div>React</div>
+      </header>
+
+      <form>
+        <div className="form">
+          <b>제목</b> <input type="text" />
+          <b>내용</b> <input type="text" />
+          <button>추가하기</button>
+        </div>
+      </form>
+      <div>
+        <h3>리스트영역입니다</h3>
+      </div>
+    </div>
+  );
 }
 
 export default App;
